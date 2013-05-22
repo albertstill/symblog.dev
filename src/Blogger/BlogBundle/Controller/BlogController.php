b4cd1af6b5f728259eec8a16ceef0d1fc69cbc4a
Albert Still <albertjamesstill@gmail.com>
2013-05-22 04:21:58 +0100

    Part 3 & 4 up untill "Adding Comments" completed
    Outside tutorial: Downloaded the pictures off demo version of symblog.co.uk

---
+++ src/Blogger/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,30 @@
+<?php
+// src/Blogger/BlogBundle/Controller/BlogController.php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Blog controller.
+ */
+class BlogController extends Controller
+{
+    /**
+     * Show a blog entry
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $blog = $em->getRepository('BloggerBlogBundle:Blog')->find($id);
+
+        $comments = $em->getRepository('BloggerBlogBundle:Comment')
+                   ->getCommentsForBlog($blog->getId());
+
+	    return $this->render('BloggerBlogBundle:Page:show.html.twig', array(
+	        'blog'      => $blog,
+	        'comments'  => $comments
+        ));
+    }
+}
